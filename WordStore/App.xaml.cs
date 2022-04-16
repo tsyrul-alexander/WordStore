0bdb1b0f074cb4cd522bb10ce1fc66675589c6b7
ocrmadmin@mts.by <ocrmadmin@mts.by>
2022-04-16 16:42:52 +0300

    initialize

---
+++ WordStore/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿namespace WordStore;
+
+public partial class App : Application {
+	public App() {
+		InitializeComponent();
+		MainPage = new MainView();
+	}
+}
---
+++ WordStore/View/MainView.xaml.cs
@@ -0,0 +1,7 @@
+namespace WordStore;
+
+public partial class MainView : ContentPage {
+	public MainView() {
+		InitializeComponent();
+	}
+}
