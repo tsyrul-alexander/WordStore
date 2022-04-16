b935529143d68e08290ed620e06f2ea4a626b7e3
ocrmadmin@mts.by <ocrmadmin@mts.by>
2022-04-16 16:43:08 +0300

    base model

---
+++ WordStore/ViewModel/BaseModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WordStore.ViewModel {
+    internal class BaseModel : INotifyPropertyChanged {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual bool SetPropertyValue<T>(ref T setProperty, T value, [CallerMemberName] string propertyName = "") {
+            if (setProperty.Equals(value)) {
+                return false;
+            }
+            setProperty = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "") {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
